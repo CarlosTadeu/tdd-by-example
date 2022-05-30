6c896ceefd635510a18d825e73d63f9e0af777ad
Carlos Tadeu <carlossantos@opus-software.com.br>
2022-05-30 16:05:38 -0300

    equality for all

--- src/test/java/tadeucarlos/MoneyTest.java
+++ src/test/java/tadeucarlos/MoneyTest.java
@@ -3,6 +3,7 @@ package tadeucarlos;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class MoneyTest {
 
@@ -10,7 +11,16 @@ class MoneyTest {
     void testMultiplication() {
         Dollar five = new Dollar(5);
 
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        five.times(3);
+        assertEquals(15, product.amount);
+    }
+
+    @Test
+    void testEquality() {
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(8));
     }
 }
