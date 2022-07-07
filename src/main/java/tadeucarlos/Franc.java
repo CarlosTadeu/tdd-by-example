978a00c581d54308595786be46dae500ad03889d
Carlos Tadeu <carlossantos@opus-software.com.br>
2022-07-07 15:19:39 -0300

    adding Franc

---
+++ src/main/java/tadeucarlos/Franc.java
@@ -0,0 +1,17 @@
+package tadeucarlos;
+
+public class Franc {
+    private int amount;
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Franc dollar = (Franc) object;
+        return amount == dollar.amount;
+    }
+}
